44d76dce39e65b79c0a3c64a3ce875946ccce304
dtio <davidtio@fosstech.biz>
2023-12-07 10:54:27 +0800

    Added audit.yml

--- audit/1.1.1.1.sh
+++ audit/1.1.1.1.sh
@@ -8,8 +8,7 @@
     then
         # Check how module will be loaded
         l_loadable="$(modprobe -n -v "$l_mname")"
-        [ "$(wc -l <<<"$l_loadable")" -gt "1" ] && l_loadable="$(
-            grep -P -- "(^\h*install|\b$l_mname)\b" <<<"$l_loadable")"
+        [ "$(wc -l <<<"$l_loadable")" -gt "1" ] && l_loadable="$(grep -P -- "(^\h*install|\b$l_mname)\b" <<<"$l_loadable")"
         if grep -Pq -- '^\h*install \/bin\/(true|false)' <<<"$l_loadable"; then
             l_output="$l_output\n - module: \"$l_mname\" is not loadable: \"$l_loadable\""
         else
